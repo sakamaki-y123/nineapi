96db8f49ba149de4084f867e937d365fb96d0fc9
sakamaki_y <sakamaki_y@worksap.co.jp>
2018-10-09 14:19:39 +0900

    fix

--- nineapi/client.py
+++ nineapi/client.py
@@ -222,7 +222,7 @@ class Client(object):
         for post in post_list:
             id = post.id
             posts_data[id] = {}
-            posts_data[id]['title'] = html.unescape(post.title)
+            posts_data[id]['title'] = html.unescape(post.title).replace("&#039;", "\'")
             posts_data[id]['url'] = post.url
             posts_data[id]['type'] = post.type
             posts_data[id]['tags'] = post.tags
